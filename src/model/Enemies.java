d27c4101777ee9c28e79e5395c05e205cdac12a3
Mika <mika.koenigsmann@std.dhsh.de>
2024-05-03 17:06:09 +0200

    Enemy Class erstellt und grundliegende Funktionen eingefügt

--- src/model/Enemies.java
+++ src/model/Enemies.java
@@ -1,4 +1,64 @@
 package model;
 
 public class Enemies {
+    private int health;
+    private int speed;
+    private int x;
+    private int y;
+
+    public Enemies(int health, int speed, int x, int y) {
+        this.health = health;
+        this.speed = speed;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+    }
+
+    public void move() {
+        this.y = y - speed; // Bewegt sich von Rechts nach Links
+    }
+
+    public boolean isOffScreen() {
+        return y < 0; //Überprüfen, ob Gegner am Ende ist, dann lose, Y-Offset muss noch an Gegnergröße angepasst werden
+    }
+
 }
