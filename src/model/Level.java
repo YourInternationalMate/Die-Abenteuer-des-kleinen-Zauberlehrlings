fa43fce0959bf62c6559e015d757f07494ca1af0
Mika <mika.koenigsmann@std.dhsh.de>
2024-05-03 17:17:21 +0200

    Level Class erstellt und grunliegende Methoden hinzugefügt

---
+++ src/model/Level.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Level {
+
+    public static final int WIDTH = 700;
+    public static final int HEIGHT = 400;
+
+    public int levelNumber;
+    public int enemyCount;
+    public int killedEnemies;
+
+    public Level(int levelNumber, int enemyCount) {
+        this.levelNumber = levelNumber;
+        this.enemyCount = enemyCount;
+        this.killedEnemies = 0;
+    }
+
+    public boolean isCompleted() { // wenn alle Gegner besiegt wurden, ist das Level geschafft
+        return killedEnemies == enemyCount;
+    }
+
+    public void enemyKilled() {
+        killedEnemies++;
+    }
+
+    public void reset() { // falls man Level neustarten möchte
+        killedEnemies = 0;
+    }
+
+    public int getLevelNumber() {
+        return levelNumber;
+    }
+
+    public int getEnemyCount() {
+        return enemyCount;
+    }
+
+    public int getKilledEnemies() {
+        return killedEnemies;
+    }
+}
