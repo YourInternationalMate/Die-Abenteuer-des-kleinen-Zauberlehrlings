b78057fc0bf9807595fb870f1392e1e86cbae50b
Mika <mika.koenigsmann@std.dhsh.de>
2024-05-05 18:26:50 +0200

    Getter für X und Y hinzugefügt + Namen für Spells entfernt

--- src/model/Spells.java
+++ src/model/Spells.java
@@ -1,16 +1,13 @@
 package model;
 
 public class Spells {
-
-    public String name;
     public String type;
     public int damage;
     public int speed;
     public int x;
     public int y;
 
-//    public Spells(String name, String type, int damage, int speed, int x, int y) { // für mehrere Typen
-//        this.name = name;
+//    public Spells(String type, int damage, int speed, int x, int y) { // für mehrere Typen
 //        this.type = type;
 //        this.damage = damage;
 //        this.speed = speed;
@@ -18,13 +15,20 @@ public class Spells {
 //        this.y = y;
 //    }
 
-    public Spells(String name, int damage, int speed, int x, int y) { // solange wir nur einen Typ haben
-        this.name = name;
+    public Spells(int damage, int speed, int x, int y) { // solange wir nur einen Typ haben
         this.type = "normal";
         this.damage = damage;
         this.speed = speed;
         this.x = x;
         this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     public void move() {
