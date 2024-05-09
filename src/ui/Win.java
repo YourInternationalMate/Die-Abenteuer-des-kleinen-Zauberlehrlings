455484fc75a1d4cd0c75fa1a1762691037507249
Mika <mika.koenigsmann@std.dhsh.de>
2024-05-09 18:23:46 +0200

    UI Classes hinzugefügt

---
+++ src/ui/Win.java
@@ -0,0 +1,30 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Win extends JPanel{
+
+    private Image backgroundImage;
+    private JFrame frame;
+
+    public Win(JFrame frame) {
+        this.frame = frame;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawBackground(g);
+    }
+
+    private void loadImages() {
+        backgroundImage = new ImageIcon("src/resources/menu/background.jpg").getImage();
+    }
+
+    private void drawBackground(Graphics g) {
+        g.drawImage(backgroundImage, 0, 0, null);
+    }
+
+
+}
